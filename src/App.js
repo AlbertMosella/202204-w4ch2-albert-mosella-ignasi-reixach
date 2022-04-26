07c06ee62f8ad499d63e58e0978f2ec72a17f82e
Ignasi Reixach <ignasireixach@gmail.com>
2022-04-26 20:44:44 +0200

    Alphabet added and letters component too

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import Hangman from "./components/Hangman/Hangman";
 import Letters from "./components/Letters/Letters";
 import Result from "./components/Result/Result";
 import UsedLetters from "./components/UsedLetters/UsedLetters";
+import alphabet from "./data/alphabet";
 
 function App() {
+  const handleClick = (clickOnLetter) => {
+    clickOnLetter.preventDefault();
+    return clickOnLetter.target.textContent;
+  };
+
   return (
     <>
       <div className="container">
@@ -14,7 +20,7 @@ function App() {
         </div>
         <GuessLetters />
         <Result />
-        <Letters />
+        <Letters alphabet={alphabet} action={handleClick} />
       </div>
     </>
   );
